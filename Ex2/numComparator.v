33a63ec5dfcb8e776e330597424cff509d896dd5
sharim <2428386483@qq.com>
2020-04-28 20:11:28 +0800

    Ex2_numComparator without testbench

--- Ex2/numComparator.v
+++ Ex2/numComparator.v
@@ -1,1 +1,12 @@
-// TODO: 4位数值比较器
+// TODO: testbench and debug
+
+module foul_bit_numComparator(
+    input  [3:0] A,
+    input  [3:0] B,
+
+    output [2:0] Y
+);
+
+    assign Y = (A>B)? 3'b001:((A=B)? 3'b010:3'b100);
+
+endmodule
