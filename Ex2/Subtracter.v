8da81c99f6e19544ca8565dfa4d1a7eda4b760d5
sharim <2428386483@qq.com>
2020-04-28 19:53:44 +0800

    Ex2_Subtracter without testbench

--- Ex2/Subtracter.v
+++ Ex2/Subtracter.v
@@ -1,1 +1,39 @@
-// TODO: 半减器、全减器、多位全减器
+// TODO: testbench and debug
+
+module half_substracter(
+    input  A,
+    input  B,
+
+    output D,
+    output J
+);
+
+    assign {D, J} = {A^B, ~A&B};
+
+endmodule
+
+module full_substracter(
+    input  A,
+    input  B,
+    input  J0,
+
+    output D,
+    output J
+);
+
+    assign {D, J} = {^{A, B, J0}, |{~A&J0, ~A&B, B&J0}};
+
+endmodule
+
+module foul_bit_full_substracter(
+    input  [3:0] A,
+    input  [3:0] B,
+    input        J0,
+
+    output [3:0] D,
+    output       J
+);
+
+    assign {J, D} = A + ~B + 5'b1_0001;
+
+endmodule
