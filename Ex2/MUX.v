fb396bfec3af674784e80b3d38a429f4857a1bab
sharim <2428386483@qq.com>
2020-04-28 18:54:14 +0800

    Ex2_MUX without testbench

--- Ex2/MUX.v
+++ Ex2/MUX.v
@@ -1,1 +1,26 @@
-// TODO: 4选1数据选择器、8选1数据选择器
+// TODO: testbench and debug
+
+module mux_4_1(
+    input       S,
+    input [3:0] D,
+    input [1:0] A,
+
+    output      Y
+);
+
+    assign Y = S? 0 : D[A];
+
+endmodule
+
+module mux_8_1_74LS151(
+    input       S,
+    input [7:0] D,
+    input [2:0] A,
+
+    output      Y,
+    output      W
+);
+
+    assign {Y, W} = S? {1'b0, 1'b1} : {D[A], ~D[A]);
+
+endmodule
